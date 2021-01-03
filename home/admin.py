5f354007bb67a81b6f94cb705d3b8e212e0ef6b0
darkun7 <182410101113@students.unej.ac.id>
2021-01-04 03:32:11 +0700

    Add models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Lab)
+admin.site.register(Training)
+admin.site.register(Attribute)
+admin.site.register(TrainingValue)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Lab(models.Model):
+    name  = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Training(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    result= models.ForeignKey(Lab ,null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+
+class Attribute(models.Model):
+    name  = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+
+class TrainingValue(models.Model):
+    training_id = models.ForeignKey(Training ,null=True, on_delete=models.SET_NULL)
+    attribute_id = models.ForeignKey(Attribute ,null=True, on_delete=models.SET_NULL)
+    value = models.FloatField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return str(self.training_id)+": "+str(self.attribute_id)+"("+str(self.value)+")"
